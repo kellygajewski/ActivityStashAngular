be35bfc09d00bd8ec33a542430a0dbce7841fca7
Kelly Gajewski <kellyagajewski@gmail.com>
2014-05-14 13:08:53 -0700

    create and read

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,6 +2,7 @@ class Activity
   include Mongoid::Document
   field :name, type: String
   field :location, type: String
+  field :link, type: String
   field :date, type: String
   field :notes, type: String
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+activities = Activity.create([{name: "Beach", location: "Santa Monica", link: "link", date: nil}])
