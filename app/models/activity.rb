24af5ab7a644263ed5ce719aa28a1fad5a1327d5
Kelly Gajewski <kellyagajewski@gmail.com>
2014-05-16 07:12:36 -0700

    bootstrap responsive table

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,6 +3,5 @@ class Activity
   field :name, type: String
   field :location, type: String
   field :link, type: String
-  field :date, type: String
   field :notes, type: String
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-activities = Activity.create([{name: "Beach", location: "Santa Monica", link: "link", date: nil}])
+# activities = Activity.create([{name: "Beach", location: "Santa Monica", link: "link", date: nil}])
